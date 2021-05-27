d9ece5eac3359d200f6488837ae9efe2374341ec
eschnei2 <everettschneider@gmail.com>
2021-05-27 13:29:18 -0500

    finished shoppingList

---
+++ shopping.js
@@ -0,0 +1,24 @@
+const food = [
+    {
+        id: 1,
+        name: "Milk",
+        price: 1.99
+    },
+    {
+        id: 2,
+        name: "Eggs",
+        price: 1.99
+    },
+    {
+        id: 3,
+        name: "Pizza rolls",
+        price: 3.99
+    },
+    {
+        id: 4,
+        name: "Chicken Breast",
+        price: 5.99
+    },
+]
+
+console.log(food)
