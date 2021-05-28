b24561b213b050b20d14b872a01ab92c4f29a1bf
eschnei2 <everettschneider@gmail.com>
2021-05-28 13:46:48 -0500

    added date created, test not real date, takes current time

--- shopping.js
+++ shopping.js
@@ -21,4 +21,52 @@ const food = [
     },
 ]
 
+const addToShoppingList = (foodObject) => {
+    const lastIndex = food.length - 1
+    const currentLastfood = food[lastIndex]
+    const maxId = currentLastfood.id
+    const idForNewfood = maxId + 1
+
+    foodObject.id = idForNewfood
+    food.push(foodObject)
+}
+
+let today = new Date()
+let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate()
+
+const beef = {
+    name: "Chuck Roast",
+    price: 8.99,
+    dateCreated: date
+}
+const bread = {
+    name: "Whole Wheat Bread",
+    price: 4,
+    dateCreated: date
+}
+const cheese = {
+    name: "Munster Cheese",
+    price: 10,
+    dateCreated: date
+}
+const potato = {
+    name: "Russet Potato",
+    price: 4.50,
+    dateCreated: date
+}
+const onion = {
+    name: "White Onion",
+    price: 6.39,
+    dateCreated: date
+}
+
+addToShoppingList(beef)
+addToShoppingList(bread)
+addToShoppingList(cheese)
+addToShoppingList(potato)
+addToShoppingList(onion)
+
+
 console.log(food)
+
+/* console.log(food.length) */ // this was an experiment
